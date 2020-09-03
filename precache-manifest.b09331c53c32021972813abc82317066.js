1c20c7485c3d44647753e12f6cdb651e80e84551
Vassiliy278 <trafaret2009@gmail.com>
2020-09-03 13:55:28 +0300

    Updates

---
+++ precache-manifest.b09331c53c32021972813abc82317066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c68791644ebb831f7c235cc67f6336d",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "3d91ea076e061b508876",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.937ebeb0.chunk.css"
+  },
+  {
+    "revision": "fe2fe02d6bec36dc9b2c",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.db817ac7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.db817ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d91ea076e061b508876",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.28228327.chunk.js"
+  },
+  {
+    "revision": "e3f60b8770ae85b5a663",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.6ec4936c.js"
+  }
+]);
